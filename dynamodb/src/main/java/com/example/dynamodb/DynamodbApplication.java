e8d4eb56663f0d8681ab7547c3ba26e496a8a0dc
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-09-21 15:57:07 -0300

    python

--- dynamodb/src/main/java/com/example/dynamodb/DynamodbApplication.java
+++ dynamodb/src/main/java/com/example/dynamodb/DynamodbApplication.java
@@ -2,12 +2,27 @@ package com.example.dynamodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class DynamodbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamodbApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate customRestTemplate(RestTemplateBuilder builder) {
+
+        return builder.setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(10))
+                .build();
+
     }
 
 }
--- dynamodb/src/main/java/com/example/dynamodb/cotacoes/modelagem/CotacaoSingleton.java
+++ dynamodb/src/main/java/com/example/dynamodb/cotacoes/modelagem/CotacaoSingleton.java
@@ -1,11 +1,27 @@
 package com.example.dynamodb.cotacoes.modelagem;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+@Component
+@Scope(value = WebApplicationContext.SCOPE_APPLICATION, proxyMode = ScopedProxyMode.TARGET_CLASS )
 public class CotacaoSingleton {
 
     CotacaoColetorFactory factory = new CotacaoColetorFactory();
+    private AtomicBoolean executando = new AtomicBoolean();
+
+    public CotacaoSingleton() {
+        executando.set(false); ;
+    }
 
     public Object cotar(Object parametros) {
 
@@ -17,28 +33,34 @@ public class CotacaoSingleton {
         tiposRetorno.add("DIESEL");
         tiposRetorno.add("SOJA_BOLSA");
 
-        //ontem e hoje
-        final List<Object> objects = buscarDynamoOntemHoje();
-        final List<Object> hoje;
-        final List<Object> ontem;
+        final boolean executando = this.executando.get();
 
-        for (String s : tiposRetorno) {
+        if(!executando) {
 
-            //if vazio buscar
-            for (Object object : objects) {
-                //filtrar tipo
-                //se conter nos dados que estavem do dynamo adiciona no retorno
-                //se nao conter busca na internet
-                // retorno sucesso internet salva no array p ir po dinamo e add no
-                // obj retorno
-                // retorno erro internet
-                // busca o de ontem
-                // nao achou de ontem
-                // retorna com valor 0
+            //ontem e hoje
+            final List<Object> objects = buscarDynamoOntemHoje();
+            final List<Object> hoje;
+            final List<Object> ontem;
+
+            for (String s : tiposRetorno) {
+
+                //if vazio buscar
+                for (Object object : objects) {
+                    //filtrar tipo
+                    //se conter nos dados que estavem do dynamo adiciona no retorno
+                    //se nao conter busca na internet
+                    // retorno sucesso internet salva no array p ir po dinamo e add no
+                    // obj retorno
+                    // retorno erro internet
+                    // busca o de ontem
+                    // nao achou de ontem
+                    // retorna com valor 0
+
+                }
 
             }
-
         }
+
 
         return null;
 
